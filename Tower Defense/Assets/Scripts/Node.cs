f7cf27b46dd885cd947925a355aad8a34f4631f3
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-06-19 19:17:14 +0200

    fix: can't construct on water Tile fron now on

--- Tower Defense/Assets/Scripts/Node.cs
+++ Tower Defense/Assets/Scripts/Node.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -13,15 +14,30 @@ public class Node : MonoBehaviour
 
     private BuildManager _buildManager;
 
+    public Material ManageableMaterial;
+
+    private string materialName;
+
     void Start()
     {
         _renderer = GetComponent<Renderer>();
         startColor = _renderer.material.color;
         _buildManager = BuildManager.instance;
+        materialName = ManageableMaterial.name;
     }
-    
+
+    private Boolean ManageableTexture()
+    {
+        return _renderer.sharedMaterial.name.Contains(materialName);
+    }
     private void OnMouseEnter()
     {
+        Debug.Log(ManageableMaterial.name);
+        Debug.Log(_renderer.sharedMaterial.name);
+        if (!ManageableTexture())
+        {
+            return;
+        }
         if (EventSystem.current.IsPointerOverGameObject())
         {
             return;
@@ -40,6 +56,10 @@ public class Node : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (!ManageableTexture())
+        {
+            return;
+        }
         if (EventSystem.current.IsPointerOverGameObject())
         {
             return;
